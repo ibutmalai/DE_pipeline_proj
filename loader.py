d1c7138a74b76364d825dbff4d7374f81af3afd5
ibutmalai <ion.butmalai@endava.com>
2023-05-31 12:33:31 +0300

    changes to branch

--- loader.py
+++ loader.py
@@ -3,12 +3,12 @@ from pandas import DataFrame
 class Loader():
     def __init__(self, data: DataFrame) -> None:
         self.data = data
-        
-    def save_csv(self, path: str):
-        self.data.to_csv(path, index=False)
-    
+            
     def save_json(self, path:str):
         self.data.to_json(path)
     
     def save_excel(self, path:str):
         pass
+    
+    def save_html(self, path:str):
+        self.data.to_html(path)
